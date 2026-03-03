beafebef67c48c826397054f9032bd2283427ab0
Егор <egor@MacBook-Air-Egor.local>
2026-03-03 18:57:11 +0700

    adfsaaa

--- FirstRGR.cpp
+++ FirstRGR.cpp
@@ -5,7 +5,7 @@
 #include <glut.h>				// Заголовочник  GLUT
 #include <algorithm>
 using namespace std;
-GLint Width = 443, Height = 333;// Начальные размеры окна
+GLint Width = 443, Height = 334;// Начальные размеры окна
 int delay = 20;				    // Задержка в МС между последними IDLE
 long time0 = 0;					// Отметка системного времени
 
