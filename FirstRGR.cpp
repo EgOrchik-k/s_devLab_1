7b335d3524674191bc2d4831b7e53a52bd91cc82
Ilya <i190907@yandex.ru>
2026-03-17 12:40:57 +0300

    sdfs

--- FirstRGR.cpp
+++ FirstRGR.cpp
@@ -8,7 +8,8 @@ using namespace std;
 GLint Width = 443, Height = 332;// Начальные размеры окна
 int delay = 20;				    // Задержка в МС между последними IDLE
 long time0 = 0;					// Отметка системного времени
-// я тоже чтото добавил ахахх
+
+
 struct Circle
 {
     float x, y;
